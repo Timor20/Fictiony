e4b9391ac03a55f8cc72e4579481e61c7c347599
Timor20 <timors294@gmail.com>
2025-09-02 17:23:59 -0700

    Finished backend files

--- backend/models/job.py
+++ backend/models/job.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 
-from backend.db.database import Base
+from db.database import Base
 
 
 class StoryJob(Base):
@@ -14,5 +14,5 @@ class StoryJob(Base):
     status = Column(String)
     story_id = Column(Integer, nullable=True)
     error = Column(String, nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=True)
-    completed_job = Column(DateTime(timezone=True), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    completed_at = Column(DateTime(timezone=True), nullable=True)
--- backend/models/story.py
+++ backend/models/story.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, J
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
-from backend.db.database import Base
+from db.database import Base
 
 class Story(Base):
     __tablename__ = "stories"
@@ -18,7 +18,7 @@ class StoryNode(Base):
     __tablename__ = "story_nodes"
 
     id = Column(Integer, primary_key=True, index=True)
-    story_id = Column(Integer, ForeignKey="stories.id", index=True)
+    story_id = Column(Integer, ForeignKey("stories.id"), index=True)
     content = Column(String)
     is_root = Column(Boolean, default=False)
     is_ending = Column(Boolean, default=False)
