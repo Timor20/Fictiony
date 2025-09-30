5429e55281b11985d22c7bdba24e558b78ec9cee
Timor20 <timors294@gmail.com>
2025-09-29 19:37:00 -0700

    Edit vite-config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig((command, mode) => {
+export default defineConfig(({command, mode}) => {
   const env = loadEnv(mode, process.cwd(), "")
 
   console.log(env.VITE_DEBUG)
@@ -10,7 +10,7 @@ export default defineConfig((command, mode) => {
   return {
     plugins: [react()],
     server: {
-      ...(env.VITE_DEBUG === "true" && {
+    ...(env.VITE_DEBUG === "true" && {
       proxy: {
         "/api": {
           target: "http://localhost:8000",
@@ -19,7 +19,6 @@ export default defineConfig((command, mode) => {
         }
       }
     }
-    ) 
-    }
+    )}
   }
 })
